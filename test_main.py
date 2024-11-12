e0904a052b312ec9efccaca3796f4d17c423e4d9
Pheryion <skengst@gmail.com>
2024-11-12 23:27:43 +0100

    Adding test_hour, test_rain_snow_boolean. Uppdating test_date_format

--- test_main.py
+++ test_main.py
@@ -2,25 +2,25 @@ from main import data_SMHI
 from datetime import datetime
 import pytest
 
+
+df, code, samlad_data_dict = data_SMHI()
 def test_API_response():
-    df, code = data_SMHI()
     assert code == 200, f"Expected status code 200, but got {code}"
 
-def test_unit():
-    df, code, samlad_data_dict = data_SMHI()
-    
-    # Check date format
+def test_date_format():
     date_str = samlad_data_dict["date"]
     try:
         datetime.strptime(date_str, '%Y-%m-%d')
     except ValueError:
         pytest.fail("Incorrect date format")
 
-     
-    # Test hour format 
+def test_hour():
+    hour = samlad_data_dict["hour"]
+    assert isinstance(hour, int) and 0 <= hour <= 23, "Hour format is incorrect"
 
-
-    # Check rainOrsnow boolean
+def test_rain_snow_boolean():
+    rain_or_snow = samlad_data_dict["rainOrSnow"]
+    assert isinstance(rain_or_snow, bool), "Rain or snow, error value"
 
 
 
